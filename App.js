e489357476ba4c683fb27573c9f52975481958c4
RustamNur <136016020+RustamNur@users.noreply.github.com>
2023-08-21 13:49:31 +0300

    dataFromMockFile

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/navbar/navbar'
+import Header from './components/header/header'
+import Main from './components/main/main'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Main />
+    </div>
+  )
+}
+
+export default App
